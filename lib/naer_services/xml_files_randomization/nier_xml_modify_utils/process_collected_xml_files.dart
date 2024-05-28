c528a9142b508f9329a4a7296686161ac60ab43f
Dusiege <dustin.siegemund@gmx.de>
2024-05-28 21:43:37 +0200

    documentation

--- lib/naer_services/xml_files_randomization/nier_xml_modify_utils/process_collected_xml_files.dart
+++ lib/naer_services/xml_files_randomization/nier_xml_modify_utils/process_collected_xml_files.dart
@@ -6,6 +6,17 @@ import 'package:NAER/naer_services/xml_files_randomization/nier_xml_modify_utils
 import 'package:NAER/nier_cli/nier_cli_fork_utils/fileTypeUtils/xml/xml_extension.dart';
 import 'package:xml/xml.dart' as xml;
 
+/// Processes a collected XML file for randomization.
+///
+/// This method reads the XML file, parses its content, and processes each action element within the XML.
+/// It checks if the action is important based on its ID, and then processes the enemy code elements accordingly.
+/// Finally, it writes the modified XML content back to the file.
+///
+/// [file] is the XML file to be processed.
+/// [sortedEnemyData] is the map of sorted enemy data.
+/// [enemyLevel] specifies the level of enemies to be modified.
+/// [enemyCategory] specifies the category of enemies to be modified.
+/// [importantIds] is the ImportantIDs object containing metadata IDs.
 Future<void> processCollectedXmlFileForRandomization(
     File file,
     Map<String, List<String>> sortedEnemyData,
